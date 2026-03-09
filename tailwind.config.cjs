0c37917ef5dde1133c04a7b75842da539c2b4b7e
Xpotato1024 <widwmitsui@gmail.com>
2026-03-09 10:12:18 +0900

    UIをSVGベースのモダンデザインへ刷新 (#8)

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./public/**/*.{html,js}",
+    "./src/**/*.{ts,js}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Outfit", '"Noto Sans JP"', "sans-serif"]
+      }
+    }
+  },
+  plugins: []
+};
